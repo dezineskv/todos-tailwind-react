6103472eb3bbd1b95625717e45ad50873c2080e8
KimV <71965590+dezineskv@users.noreply.github.com>
2025-01-28 15:52:50 -0800

    index css for tailwind moved

--- src/main.jsx
+++ src/main.jsx
@@ -1,6 +1,6 @@
 import { createContext, StrictMode, useState } from "react";
 import { createRoot } from "react-dom/client";
-import "./index.css";
+import "./assets/index.css";
 import App from "./App.jsx";
 import { BrowserRouter } from "react-router-dom";
 import toast, { Toaster } from "react-hot-toast";
@@ -8,7 +8,7 @@ import toast, { Toaster } from "react-hot-toast";
 createRoot(document.getElementById("root")).render(
   <StrictMode>
     <BrowserRouter>
-    <Toaster></Toaster>
+      <Toaster></Toaster>
       <App />
     </BrowserRouter>
   </StrictMode>
