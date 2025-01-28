61a59085bca358e5a38d864c87cfcbc563b44c68
KimV <71965590+dezineskv@users.noreply.github.com>
2025-01-28 15:38:38 -0800

    tailwind css config update

--- vite.config.js
+++ vite.config.js
@@ -5,4 +5,9 @@ import react from "@vitejs/plugin-react";
 export default defineConfig({
   plugins: [react()],
   base: "/todos-tailwind-react.git/",
+  css: {
+    postcss: {
+      plugins: [tailwindcss()],
+    },
+  },
 });
