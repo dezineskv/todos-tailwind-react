fc1bf62b473fe0e91728f2dfb3e816b2b8803c79
KimV <71965590+dezineskv@users.noreply.github.com>
2025-06-21 17:40:04 -0700

    vite config update for ghpages

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,6 @@ import tailwindcss from "@tailwindcss/vite";
 
 // https://vite.dev/config/
 export default defineConfig({
+  base: "/todos-tailwind-react/",
   plugins: [react(), tailwindcss()],
 });
